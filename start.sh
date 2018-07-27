6389d3b09327ae5bc8ff0783da6aec36c555c95a
DunkelAmber <frama95@gmail.com>
2018-07-27 09:42:17 +0200

    feat: add gstreamer command

---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Start GStreamer streaming
+echo "GStreamer: starting streaming"
+gst-launch-1.0 v4l2src ! videoconvert ! video/x-raw ! jpegenc ! rtpjpegpay ! udpsink host=10.0.0.101 port=50200
