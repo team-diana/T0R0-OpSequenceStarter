ee89e2c5b921e9c6553ac1703d0cd38915415fee
DunkelAmber <frama95@gmail.com>
2018-07-27 10:22:05 +0200

    fix: loop and input

--- start.sh
+++ start.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 # Command-line user interface to start sequences
 
-echo "Enter a number between 1 and 10. "
-echo "[0] -> ALL"
-echo "[1] -> GStreamer"
-echo "[q] -> QUIT"
-read NUM
-
 LOOP=1
 
 while [[ $LOOP -eq 1 ]] ; do
+    echo "Enter a number between 1 and 10. "
+    echo "[0] -> ALL"
+    echo "[1] -> GStreamer"
+    echo "[q] -> QUIT"
+    read NUM
     case $NUM in
         0) command "sh commands.sh all" ;;
     	1) command "sh commands.sh gstreamer" ;;
@@ -23,8 +22,10 @@ while [[ $LOOP -eq 1 ]] ; do
     	8) echo "eight" ;;
     	9) echo "nine" ;;
     	10) echo "ten" ;;
+
         "q") LOOP=0 ;;
         "Q") LOOP=0 ;;
+
     	*) echo "INVALID NUMBER!" ;;
     esac
 done
