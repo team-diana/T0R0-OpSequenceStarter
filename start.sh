58c261b3f81b9bfb794e8653392f3c6ea0eb74e1
Francesco Masciari <frama95@gmail.com>
2018-07-28 13:01:34 +0200

    feat: add altec and date

--- start.sh
+++ start.sh
@@ -8,6 +8,8 @@ while [ $LOOP -eq 1 ] ; do
     echo "Enter a number between 1 and 10. "
     echo "[0] -> ALL"
     echo "[1] -> GStreamer"
+    echo "[2] -> Altec"
+    echo "[9] -> Change system date"
     echo "[q] -> QUIT"
     echo -n " > "
     read NUM
@@ -16,14 +18,14 @@ while [ $LOOP -eq 1 ] ; do
         0) sh commands.sh all ;;
     	1) sh commands.sh gstreamer ;;
 
-    	2) echo "two" ;;
+    	2) sh commands.sh altec ;;
     	3) echo "three" ;;
     	4) echo "four" ;;
     	5) echo "five" ;;
     	6) echo "six" ;;
     	7) echo "seven" ;;
     	8) echo "eight" ;;
-    	9) echo "nine" ;;
+    	9) sh commands.sh date ;;
     	10) echo "ten" ;;
 
         "q") LOOP=0 ;;
