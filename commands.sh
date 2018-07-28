ba8e34048cfdf6ef86b5b9b3816f2871c9d1bcae
Francesco Masciari <frama95@gmail.com>
2018-07-28 13:00:03 +0200

    feat: add altec and date

--- commands.sh
+++ commands.sh
@@ -16,7 +16,19 @@ for var in "$@"
             "gstreamer") # Start GStreamer streaming
             echo "GStreamer: starting streaming"
             gst-launch-1.0 v4l2src ! videoconvert ! video/x-raw ! jpegenc ! rtpjpegpay ! udpsink host=10.0.0.101 port=50200;;
-
+            
+            "altec") # Start Mobility and Arm driver developed for Altec
+            echo "Altec: starting Mobility and Arm driver"
+            cd ~/salone-orientamento-demo/altec/build && cmake.. && make && ./altec ;;
+            
+            "date")
+            echo "Set current date [yyyy mm dd hh mm]"
+            echo -n " > "
+            read YEAR MONTH DAY HOUR MIN
+            months=( ["01"]="Jan" ["02"]="Feb" ["03"]="Mar" ["04"]="Apr" ["05"]="May" ["06"]="Jun" ["07"]="Jul" ["08"]="Agu" ["09"]="Sep" ["10"]="Oct" ["11"]="Nov" ["12"]="Dec")
+            echo "${DAY} ${months[${MONTH}]} "${YEAR} ${HOUR}:${MIN}:30"
+            #sudo date --set="${DAY} ${months[${MONTH}]} "${YEAR} ${HOUR}:${MIN}:30"
+            
             *) echo "INVALID ARGUMENT!" ;;
         esac
 
